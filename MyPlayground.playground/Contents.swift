be3c262f469b54f6e58b9e529c55cf155956644c
Роман Воробец <vorobetsroman@mail.ru>
2021-01-13 16:40:21 +0300

    lesson2

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,34 +1,100 @@
 import Cocoa
-// 1. Решить квадратное уравнение
-let a: Double = 4.0
-let b: Double = 20.0
-let c: Double = 4.0
 
-let d: Double = b * b - 4 * a * c
+//1. Написать функцию, которая определяет, четное число или нет
 
-if d < 0 {
-    let str = "unreal"
-} else if d == 0 {
-    let x = -b / 2 * a
-} else if d > 0 {
-    let x1 = (-b - sqrt(d)) / 2 * a
-    let x2 = (-b + sqrt(d)) / 2 * a
+func isOdd(val: Int) -> Bool {
+    if (val % 2 > 0) {
+        return false
+    } else {
+        return true
+    }
 }
 
-//2. Даны катеты прямоугольного треугольника. Найти площадь, периметр и гипотенузу треугольника.
+let odd = isOdd(val: 4)
+let notOdd = isOdd(val: 5)
+//2. Написать функцию, которая определяет, делится ли число без остатка на 3
 
-let kat1: Float = 3
-let kat2: Float = 4
+func isDividedBy_3(val: Int) -> Bool {
+    if (val % 3 > 0) {
+        return false
+    } else {
+        return true
+    }
+}
 
-let area = kat1 * kat2 / 2
-let perimeter = kat1 + kat2 + sqrt(kat1 * kat1 + kat2 * kat2)
-let hypotenuse = sqrt(kat1 * kat1 + kat2 * kat2)
+let isTrue = isDividedBy_3(val: 27)
+let isFalse = isDividedBy_3(val: 41)
+//3. Создать возрастающий массив из 100 чисел
+// Удалить из массива все четные числа и все числа, которые не делятся на 3
 
-//3. Пользователь вводит сумму вклада в банк и годовой процент. Найти сумму вклада через 5 лет
+func createArray(size: Int) -> [Int] {
+    var arr:[Int] = []
+    for val in 1...size {
+        arr.append(val)
+    }
+    return arr
+}
 
-let deposit: Float = 1000
-let percent: Float = 1.05
-let time: Float = 5
-let result = deposit * pow(percent, time)
+func removeOddAnd_3(_arr: [Int]) -> [Int] {
+    var arr = [Int]()
+    for val in _arr where !isOdd(val: val) && !isDividedBy_3(val: val) {
+        arr.append(val)
+    }
+    return arr
+}
+
+let arr = createArray(size: 100)
+let resultArr = removeOddAnd_3(_arr: arr)
+//4. Написать функцию, которая добавляет в массив новое число Фибоначчи и добавить при помощи нее 100 элементов (Fn = Fn1-1 + Fn-2)
 
 var str = "Hello, playground"
+
+//Old:
+//1. Написать функцию, которая определяет четное число или нет.
+func isOdd(val: Int) ->Bool  {
+    return (val % 2) == 0
+}
+let odd:Bool = isOdd(val: 5)
+print(odd)
+//2. Написать функцию, которая определяет, делится ли число без остатка на 3.
+func divizibleOn3(val: Int) ->Bool {
+    return (val % 3) == 0
+}
+let on3 = divizibleOn3(val: 15)
+print(on3)
+//3. Создать возрастающий массив из 100 чисел.
+var array:[Int] = []// = Array(repeating: 0, count: 100)
+var variable: Int = 0
+for _ in 1...100 {
+    variable += Int.random(in: 1...5)
+    array.append(variable)
+}
+print("возрастающий массив из 100 чисел")
+print(array)
+
+//Удалить из этого массива все четные числа и все числа, которые не делятся на 3.
+for (index, item) in array.enumerated().reversed() {
+    if (isOdd(val:item) || divizibleOn3(val:item)) {
+        array.remove(at: index)
+    }
+}
+print("массив без чётных и делящихся на 3")
+print(array)
+
+//4. * Написать функцию, которая добавляет в массив новое число Фибоначчи и добавить при помощи нее 100 элементов:
+func addFibIntoArr(arr:[Int64]) ->[Int64] {
+    var outArr = arr
+    if arr.count > 1 {
+        outArr.append(arr.last! + arr[arr.count - 2])
+    } else {
+        outArr.append(1)
+    }
+    return outArr
+}
+
+var fibArr:[Int64] = []
+print(INT64_MAX)
+for _ in 0...80 {
+    fibArr = addFibIntoArr(arr: fibArr)
+}
+print(fibArr)
